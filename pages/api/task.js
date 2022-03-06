19aee86bd7606f1d4bd8fcd6130d084a07458e9e
Bridget Turnbull <bridget@192-168-1-108.tpgi.com.au>
2022-03-06 16:01:30 +1100

    test cron

--- pages/api/task.js
+++ pages/api/task.js
@@ -1,4 +1,5 @@
 export default function handler(req, res) {
-    console.log("cron running");
-    res.status(200).json("cron running");
+    let sydneyTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Sydney"});
+    console.log("cron running at " + sydneyTime);
+    res.status(200).json("cron running at " + sydneyTime);
   }
